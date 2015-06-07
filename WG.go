6d3241398bf372464a0ac69ff25d66ae2eebe6d4
HowTheStoryEnds <harald.brinkhof@gmail.com>
2015-06-07 02:09:41 +0200

    initial commit

---
+++ WG.go
@@ -0,0 +1,79 @@
+﻿package github.com/howthestoryends/WG
+import(
+   "net/http"
+   "io/ioutil"
+)
+
+// we request our data from WG server, we allow for net/http
+type WG struct {
+   region string
+   transport string
+   method string
+   apiKey string
+}
+func (w *WG) Init(region string,transport string,method string, ApiKey string){
+     w.SetRegion(region)
+     w.SetTransport(transport)
+     w.SetMethod(method)
+     w.SetApiKey(ApiKey)
+}
+
+
+func (w *WG) constructURL() string{
+     //sane defaults
+     if(w.transport != "http"){ w.transport = "https" }
+
+     var regional = make(map[string]string) 
+     regional["eu"] = "api.worldoftanks.eu/wot/"
+     regional["na"] = "api.worldoftanks.com/wot/"
+     regional["ru"] = "api.worldoftanks.ru/wot/"
+                
+      
+     return w.transport + "://" + regional[w.region]
+    
+}
+
+func (w *WG) retrieveData(action string, parameters map[string]string) string{
+
+    var val,_ = http.Get("https://api.mybiz.com/articles.json")
+    var res,_ = ioutil.ReadAll(val.Body)
+    val.Body.Close()
+
+    return string(res)
+}
+
+func (w *WG) SetRegion(val string){
+     w.region = val
+}
+func (w *WG) SetTransport(val string){
+     w.transport = val
+}
+func (w *WG) SetMethod(val string){
+     w.method = val
+}
+func (w *WG) SetApiKey(val string){
+     w.apiKey = val
+}
+
+type Tank struct {
+    Name string
+    InGarage bool
+    AvgDmg uint32
+    AvgSpotting uint32
+    AvgExp uint32
+}
+
+
+type Player struct {
+    id uint32
+    nickname string
+    region string
+    Tanks []Tank
+}
+
+
+func (w* WG) GetPlayer(ApiKey string, player uint32) Player {
+   return Player{}
+}
+
+
---
+++ WG_test.go
@@ -0,0 +1,57 @@
+﻿package github.com/howthestoryends/WG
+
+import(
+    "testing"
+    . "gopkg.in/check.v1"
+    "github.com/jarcoal/httpmock"
+    "net/http"
+    "io/ioutil"
+   
+)
+
+
+
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+
+type WGSuite struct{
+     Wg WG
+}
+
+var _ = Suite(&WGSuite{})
+
+func (s *WGSuite) SetUpSuite(c *C){
+    s.Wg.Init("na","https","get","apikey")
+}
+
+func (s *WGSuite) TestConstructURL(c *C) {
+    s.Wg.SetRegion("na")
+    c.Check(s.Wg.constructURL(), Equals,"https://api.worldoftanks.com/wot/")    
+    s.Wg.SetRegion("eu")
+    c.Check(s.Wg.constructURL(), Equals,"https://api.worldoftanks.eu/wot/")
+    s.Wg.SetTransport("http")
+    c.Check(s.Wg.constructURL(), Equals,"http://api.worldoftanks.eu/wot/")
+    c.Check(42, Equals, 42)
+    c.Check(42, Equals, 42)
+    
+}
+
+
+func (s *WGSuite) TestHelloWorld2(c *C) {
+    
+    httpmock.Activate()
+    defer httpmock.DeactivateAndReset()
+
+    httpmock.RegisterResponder("GET", "https://api.mybiz.com/articles.json",
+        httpmock.NewStringResponder(200, `[{"id": 1, "name": "My Great Article"}]`))
+    //var hc = http.Client{}
+    var val,_ = http.Get("https://api.mybiz.com/articles.json")
+    var _,_ = ioutil.ReadAll(val.Body)
+    val.Body.Close()
+    c.Check(42, Equals, 42)
+   
+    
+}
+
